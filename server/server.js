7700d078cab2ba94983b18c1eaf26c1aa903eb8b
yelo12324 <yeloindia.contact@gmail.com>
2025-08-02 14:58:53 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // 🔧 Allow React frontend on port 5173 and 5174
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://gunno-media-production-wxet.vercel.app/'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://gunno-media-production-wxet.vercel.app/', 'https://gunnomediaproductions.com'] 
 }));
 
 app.use(bodyParser.json());
